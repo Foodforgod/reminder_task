c45ba442fd01f9e0465adb31925548ec5e1737b4
Yong Gene <118701792+Foodforgod@users.noreply.github.com>
2026-07-02 14:42:31 +0800

    Update dashboad.php

--- dashboad.php
+++ dashboad.php
@@ -1,79 +1,65 @@
-<?php
-include "db.php";
+<?php 
+include 'db.php';
 
-if (isset($_GET['action']) && $_GET['action'] == 'delete'){ 
- $id = $_GET['id'];
- $qry="DELETE FROM tasks WHERE Id=$id"; 
- $conn->query($qry); 
- echo"<script>alert('Task deleted successfully');
-    window.location.href='dashboad.php';
-    </script>";
+if (isset($_GET['action']) && $_GET['action'] == 'delete') {
+	$id=$_GET['id'];
+	$qry="DELETE FROM attendante where id='$id'";
+	$sttr=$conn->query($qry);
+	echo "<script>alert('delete complete');
+	window.location.href='Stundent_index.php';
+	</script>";
+}
+
+$select="SELECT * FROM attendante";
+$sttr=$conn->query($select);
+$num=$sttr->num_rows;
+
+if (isset($_GET['search'])) {
+	$select="SELECT * FROM attendante where date(date_attendante)='$_GET[search]'";
+    $sttr=$conn->query($select);
 
 }
 
- $select= "SELECT * FROM `tasks`";
- $sttr=$conn->query($select);
- $num=$sttr->num_rows; 
-
- if(isset($_GET['search_box'])){ 
- $select= "SELECT * FROM `tasks` WHERE Task like '%".$_GET['search_box']."%'";
- $sttr=$conn->query($select);
- }
- 
 
 
-
-?>
+ ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>Document</title> 
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Stundent attendante system</title>
 </head>
 <body>
-<form method=get action="">
-<input type=text name="search_box" /> 
-<button type=submit name="search">search</button> 
-</form>
-<br>
-<a href="add_task.php">add task</a>
-<head>
-    <body>
+	<form method="get" action="">
+		<input type="date" name="search">
+		<button type="submit" name="btn">search</button>
+	</form>
+	<a href="add.php">add</a>
+	<table>
+	<thead>
+		<th>Stundent name</th>
+		<th>gender</th>
+		<th>phone</th>
+		<th>date-attendante</th>
+	</thead>
+	<tbody>
+		<?php while($row=$sttr->fetch_assoc()) { ?>
+		<tr>
+			<td><?=$row['stundent_name']?></td>
+			<td><?=$row['gender']?></td>
+			<td><?=$row['phone']?></td>
+			<td><?=$row['date_attendante']?></td>
+			<td>
+				<a href="edit.php?id=<?=$row['id']?>">edit</a>
+				<a href="Stundent_index.php?action=delete&id=<?=$row['id']?>">delete</a>
 
-    
-<table> 
-    <thead> 
-        <th>Id</th> 
-        <th>Task</th>
-        <th>remind_date</th>    
-        <th>Desc</th> 
-    </thead> 
-    <tbody>
-        <?php while($row=$sttr->fetch_assoc()){ ?>
-            <tr> 
-            <td><?=$row['Id']?></td>
-            <td><?=$row['Task']?></td> 
-            <td><?=$row['remind_date']?></td>
-            <td><?=$row['Desc']?></td>
-            <td>
-               <a href="edit.php?id=<?=$row['Id']?>">edit</a>
-               <a href="dashboad.php?action=delete&id=<?=$row['Id']?>">delete</a>
-            </td>
-     </tr>
-      <?php } ?> 
-    </tbody> 
-    
-                   
-                
-          
-</table>  
+			</td>
+		</tr>
+	<?php } ?>
+	</tbody>
+	</table>
 
-</td> 
 
 </body>
-
-</head>
-
 </html>
